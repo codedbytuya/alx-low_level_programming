2a075864cb6c0387e11799ee0797e38b1182ca43
codedbytuya <tuyambeka08@gmail.com>
2023-06-20 16:39:40 -0700

    sign edit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,18 @@
 int print_sign(int n)
 {
     if (n > 0)
+    {
         return 1;
         putchar('+');
+    }
     else if(n == 0 )
+    {
         return 0;
         putchar('0');
+    }
     else
+    {
         return -1;
         putchar('-')
+    }
 }
