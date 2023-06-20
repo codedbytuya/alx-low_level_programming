8c18b89340dcd55064b9d11cab30f498f63e11e5
codedbytuya <tuyambeka08@gmail.com>
2023-06-20 15:46:08 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+    uint8_t times;
+    for (times = 0 ; letter <=9; letter++){
+	    for (letter = 'a'; letter <= 'z'; letter++){
+		    _putchar(letter);
+	    _putchar('\n');
+	    }
+    }
+}
