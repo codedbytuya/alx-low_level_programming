b8153741da0a3f867efcb2f06291a329812bd142
codedbytuya <tuyambeka08@gmail.com>
2023-06-20 16:28:24 -0700

    is alpa edit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,12 @@
  * Returns 1 if c is a letter, lowercase or uppercase
  *Returns 0 otherwise
  */
- 
+
 int _isalpha(int c)
 {
-    if (isalpha(c))
-        return 1;
-    else
-        return 0;
-}
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+} 
