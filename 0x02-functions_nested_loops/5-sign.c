e2a2b8bc2cb05f6261a67014721fb345899462de
codedbytuya <tuyambeka08@gmail.com>
2023-06-20 16:44:32 -0700

    aaaarh, silly mistakes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,22 +6,21 @@
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
  */
- 
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        return 1;
-        putchar('+');
-    }
-    else if(n == 0 )
-    {
-        return 0;
-        putchar('0');
-    }
-    else
-    {
-        return -1;
-        putchar('-');
-    }
-}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}	
