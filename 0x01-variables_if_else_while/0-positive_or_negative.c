0b7a75ab3184af9fca468d51fbfc52219372295a
codedbytuya <tuyambeka08@gmail.com>
2023-06-19 17:47:08 -0700

    0 more error fixes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,8 @@ int main()
 {
     srand(time(NULL)); /*Seed the random number generator with the current time*/
 
-    int n = rand() % 201 - 100; /*Generate a random number between -100 and 100*/
+    int n;
+    n = rand() % 201 - 100; /*Generate a random number between -100 and 100*/
 
     if (n == 0) {
         printf("%d is zero", n);
