9f5860572eb1cf91f52b6d905939bfede8aa756e
codedbytuya <tuyambeka08@gmail.com>
2023-06-26 16:03:26 -0700

    reset to 98 and main

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - Take the pointer updates the value pointed to
+ *@n: int to change
+ *Written by Valombola Tuyambeka
+ *Return: 0 is success
+ */
+void reset_to_98(int *n){
+	*n =98 ;		
+}
