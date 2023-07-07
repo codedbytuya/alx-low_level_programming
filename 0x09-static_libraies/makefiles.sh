d670fbac4e4b30e0f6575fd671942cac92084dfa
codedbytuya <tuyambeka08@gmail.com>
2023-07-07 16:40:34 -0700

    files added

---
+++ 0x09-static_libraies/makefiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo '#include "main.h"' > _putchar.c
+echo '#include "main.h"' > _islower.c
+echo '#include "main.h"' > _isalpha.c
+echo '#include "main.h"' > _abs.c
+echo '#include "main.h"' > _isupper.c
+echo '#include "main.h"' > _isdigit.c
+echo '#include "main.h"' > _strlen.c
+echo '#include "main.h"' > _puts.c
+echo '#include "main.h"' > _strcpy.c
+echo '#include "main.h"' > _atoi.c
+echo '#include "main.h"' > _strcat.c
+echo '#include "main.h"' > _strncat.c
+echo '#include "main.h"' > _strncpy.c
+echo '#include "main.h"' > _strcmp.c
+echo '#include "main.h"' > _memset.c
+echo '#include "main.h"' > _memcpy.c
+echo '#include "main.h"' > _strchr.c
+echo '#include "main.h"' > _strspn.c
+echo '#include "main.h"' > _strpbrk.c
+echo '#include "main.h"' > _strstr.c
