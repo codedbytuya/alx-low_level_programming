f7d7c27a818a2230ca1796c0c07f251cb9b07fab
codedbytuya <tuyambeka08@gmail.com>
2023-07-12 13:02:12 -0700

    whatsmyname

---
+++ 0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+    printf("%s", argv[0]);
+    return 0;
+}
