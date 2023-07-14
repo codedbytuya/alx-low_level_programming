6dbb23a174d5816e376522cae7097acdf926fb6c
codedbytuya <tuyambeka08@gmail.com>
2023-07-13 17:04:01 -0700

    fixing c++ style comments

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,24 +13,23 @@ int *array_range(int min, int max)
     int *arr;
     int size, i;
 
-    // Check if min is greater than max
+    /* Check if min is greater than max */
     if (min > max)
         return NULL;
 
-    // Calculate the size of the array
+    /* Calculate the size of the array */
     size = max - min + 1;
 
-    // Allocate memory for the array
+    /* Allocate memory for the array */
     arr = malloc(sizeof(int) * size);
 
-    // Check if malloc failed
+    /* Check if malloc failed */
     if (arr == NULL)
         return NULL;
 
-    // Initialize the array with values from min to max
+    /* Initialize the array with values from min to max */
     for (i = 0; i < size; i++)
         arr[i] = min++;
 
     return arr;
 }
-
